aac3ff8171c76441deed527addb2e038617e5d96
Vladimir Zaikovskii <kennyvova@ya.ru>
2022-12-15 00:03:16 +0300

    init

---
+++ script.js
@@ -0,0 +1,60 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let ctx = canvas.getContext("2d");
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+    this.vx = (Math.random() - 0.5) * 3;
+    this.vy = (Math.random() - 0.5) * 3;
+    this.r = Math.random() * 5;
+  }
+
+  run() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x < 0 || this.x > width) {
+      this.vx *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.vy *= -1;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "#cccccc";
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+let particles = new Array(100).fill(0).map(() => new Particle());
+
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, width, height);
+
+  particles.forEach((particle) => {
+    particle.run();
+    particle.draw();
+  });
+}
+
+animate();
